5806c80b3c756d322ae3783cf6b2d6cac658dd9b
AristonCatipay <89591363+AristonCatipay@users.noreply.github.com>
2022-01-06 12:22:01 +0800

    Fix History

--- file_set/home-student-instructor/student/nav-bar-links/student-account-history.php
+++ file_set/home-student-instructor/student/nav-bar-links/student-account-history.php
@@ -4,6 +4,7 @@ session_start();
     include '../home-student-backend.php';
     include '../../../database/database_connection.php';
     $user_data = check_if_student_login($con);
+    $student_number = $user_data["student_number"];
 ?>
 <body>
 
@@ -19,7 +20,32 @@ session_start();
         <th style="width:20%">Date Returned</th>
         <th style="width:8%">Penalty</th>
       </tr>
+      <?php
+        $query = "SELECT br.*, b.book_title
+        FROM borrow_request_student br, book b, students s
+        WHERE br.book_id=b.book_id AND br.student_number=s.student_number AND br.student_number=$student_number 
+        AND request_status='RETURNED';
+        ";
+        $result = mysqli_query($con,$query);
+        while ($data = mysqli_fetch_array($result)){
+          $transaction_id = $data["transaction_id"];
+          $book_title = $data["book_title"];    
+          $transaction_date  = $data["transaction_date"];
+          $pickup_date = $data["pickup_date"];
+          $request_type = $data["request_status"];
+          $return_due_date = $data["return_due_date"];
+          $return_date = $data["return_date"];
+          $student_number = $data["student_number"];
+      ?>
+          <tr>
+            <td><?php echo $book_title?></td>
+            <td><?php echo $transaction_date?></td>
+            <td><?php echo $return_due_date?></td>
+            <td><?php echo $return_date?></td>
+          </tr>
+      <?php
+        }
+    ?>
     </table>
-
 </body>
 </html>
