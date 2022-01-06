f64eefdc107c43a9fa2961e354926bc81ff16898
AristonCatipay <89591363+AristonCatipay@users.noreply.github.com>
2022-01-06 13:37:15 +0800

    Added Change Password Function for Student and Instructor

--- file_set/home-student-instructor/instructor/nav-bar-links/instructor-account-profile.php
+++ file_set/home-student-instructor/instructor/nav-bar-links/instructor-account-profile.php
@@ -1,7 +1,16 @@
 <?php
 session_start();
     include '../instructor-nav-bar.php';
+    include '../home-instructor-backend.php';
     include '../../../database/database_connection.php';
+    $user_data = check_if_instructor_login($con);
+    $instructor_name = $user_data["instructor_name"];
+    $instructor_number = $user_data["instructor_number"];
+    $instructor_department = $user_data["instructor_department"];
+    $instructor_email = $user_data["instructor_email"];
+    $instructor_contact = $user_data["instructor_contact"];
+    $instructor_password = $user_data["instructor_password"];
+    
 ?>
 <body>
     <div class="account-header">
@@ -20,11 +29,11 @@ session_start();
       </div>
 
       <div class="right">
-        <div class="details"><p style="font-size: 15px; margin-left:10px;"></p></div>
-        <div class="details"><p style="font-size: 15px; margin-left:10px;"></p></div>
-        <div class="details"><p style="font-size: 15px; margin-left:10px;"></p></div>
-        <div class="details"><p style="font-size: 13px; margin-left:10px;"></p></div>
-        <div class="details"><p style="font-size: 15px; margin-left:10px;">+63</p></div>
+        <div class="details"><p style="font-size: 15px; margin-left:10px;"><?php echo $instructor_name?></p></div>
+        <div class="details"><p style="font-size: 15px; margin-left:10px;"><?php echo $instructor_number ?></p></div>
+        <div class="details"><p style="font-size: 15px; margin-left:10px;"><?php echo $instructor_department?></p></div>
+        <div class="details"><p style="font-size: 13px; margin-left:10px;"><?php echo $instructor_email?></p></div>
+        <div class="details"><p style="font-size: 15px; margin-left:10px;">+639<?php echo $instructor_contact?></p></div>
         <div class="pass-change"><a href="#" id="change-pass">Change Password?</a></div>
       </div>
     </div>
@@ -42,13 +51,14 @@ session_start();
           </div>
 
           <div class="change-right">
-            <input type=password class="change-details"></input>
-            <input type=password class="change-details"></input>
-            <input type=password class="change-details"></input>
+            <form action="instructor-account-profile.php" method="post">
+              <input type=password class="change-details" name="current_password"></input>
+              <input type=password class="change-details" name="new_password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,31}" required title="that must contain 8 or more characters that are of at least one number, and one uppercase and lowercase letter"></input>
+              <input type=password class="change-details" name="confirm_new_password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,31}" required title="that must contain 8 or more characters that are of at least one number, and one uppercase and lowercase letter"></input>
           </div>
-          <button class="save-change" id="change-save">Save</a></button>
+              <button class="save-change" id="change-save" name="change_password">Save</button>
+            </form>
         </div>
-
     </div>
 </body>
 </html>
@@ -72,3 +82,33 @@ session_start();
       }
     }
 </script>
+
+<?php
+// Change Password
+
+  if(isset($_POST["change_password"])){
+    $current_password = md5($_POST["current_password"]);
+    $new_password = md5($_POST["new_password"]);
+    $confirm_new_password = md5($_POST["confirm_new_password"]);
+
+    if(isset($current_password) && isset($new_password) && isset($confirm_new_password)){
+      if($current_password === $instructor_password){
+        if ($new_password === $confirm_new_password){
+          $query =  "UPDATE instructors SET instructor_password='$new_password' WHERE instructor_number=$instructor_number";
+          mysqli_query($con,$query);
+          echo '<script type="text/javascript">alert("SUCCESSFUL!"); 
+            location="instructor-account-profile.php"; </script>';
+        }else{
+          echo '<script type="text/javascript">alert("NEW PASSWORD DOESNT MATCH!"); 
+            location="instructor-account-profile.php"; </script>';
+        }
+      }else{
+        echo '<script type="text/javascript">alert("WRONG OLD PASSWORD!"); 
+            location="instructor-account-profile.php"; </script>';
+      }
+    }
+    else{
+      echo '<script type="text/javascript">alert("DATA INCOMPLETE!"); 
+            location="instructor-account-profile.php"; </script>';
+    }
+  }
--- file_set/home-student-instructor/student/nav-bar-links/student-account-profile.php
+++ file_set/home-student-instructor/student/nav-bar-links/student-account-profile.php
@@ -32,7 +32,7 @@ session_start();
         <div class="details"><p style="font-size: 15px; margin-left:10px;"><?php echo $student_number?></p></div>
         <div class="details"><p style="font-size: 15px; margin-left:10px;"><?php echo $student_course?></p></div>
         <div class="details"><p style="font-size: 13px; margin-left:10px;"><?php echo $student_email?></p></div>
-        <div class="details"><p style="font-size: 15px; margin-left:10px;">+63<?php echo $student_contact?></p></div>
+        <div class="details"><p style="font-size: 15px; margin-left:10px;">+639<?php echo $student_contact?></p></div>
         <div class="pass-change"><a href="#" id="change-pass">Change Password?</a></div>
       </div>
     </div>
@@ -42,7 +42,6 @@ session_start();
           <span class="close">&times;</span>
           <div class="change-header">Change Password</div>
 
-
           <div class="change-left">
             <div class="change-details-label"><label>Current Password:</label></div>
             <div class="change-details-label"><label>New Password:</label></div>
@@ -50,13 +49,14 @@ session_start();
           </div>
 
           <div class="change-right">
-            <input type=password class="change-details"></input>
-            <input type=password class="change-details"></input>
-            <input type=password class="change-details"></input>
+            <form action="student-account-profile.php" method="post">
+              <input type=password class="change-details" name="current_password"></input>
+              <input type=password class="change-details" name="new_password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,31}" required title="that must contain 8 or more characters that are of at least one number, and one uppercase and lowercase letter"></input>
+              <input type=password class="change-details" name="confirm_new_password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,31}" required title="that must contain 8 or more characters that are of at least one number, and one uppercase and lowercase letter"></input>
           </div>
-          <button class="save-change" id="change-save">Save</a></button>
+              <button class="save-change" id="change-save" name="change_password">Save</button>
+            </form>
         </div>
-
     </div>
 </body>
 </html>
@@ -80,3 +80,34 @@ session_start();
       }
     }
 </script>
+
+
+<?php
+// Change Password
+
+  if(isset($_POST["change_password"])){
+    $current_password = md5($_POST["current_password"]);
+    $new_password = md5($_POST["new_password"]);
+    $confirm_new_password = md5($_POST["confirm_new_password"]);
+
+    if(isset($current_password) && isset($new_password) && isset($confirm_new_password)){
+      if($current_password === $student_password){
+        if ($new_password === $confirm_new_password){
+          $query =  "UPDATE students SET student_password='$new_password' WHERE student_number=$student_number";
+          mysqli_query($con,$query);
+          echo '<script type="text/javascript">alert("SUCCESSFUL!"); 
+            location="student-account-profile.php"; </script>';
+        }else{
+          echo '<script type="text/javascript">alert("NEW PASSWORD DOESNT MATCH!"); 
+            location="student-account-profile.php"; </script>';
+        }
+      }else{
+        echo '<script type="text/javascript">alert("WRONG OLD PASSWORD!"); 
+            location="student-account-profile.php"; </script>';
+      }
+    }
+    else{
+      echo '<script type="text/javascript">alert("DATA INCOMPLETE!"); 
+            location="student-account-profile.php"; </script>';
+    }
+  }
